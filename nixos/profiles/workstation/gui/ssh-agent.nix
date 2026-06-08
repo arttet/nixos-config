b8e9f1e821c908024e3891d5a97f7a7f0123e3dd
Artyom Tetyukhin <51746822+arttet@users.noreply.github.com>
2026-06-08 23:47:40 +0400

    fix(ssh): expose agent socket to desktop sessions

--- nixos/profiles/workstation/gui/ssh-agent.nix
+++ nixos/profiles/workstation/gui/ssh-agent.nix
@@ -5,4 +5,6 @@
     enableAskPassword = true;
     askPassword = "${pkgs.kdePackages.ksshaskpass}/bin/ksshaskpass";
   };
+
+  environment.sessionVariables.SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/ssh-agent";
 }
