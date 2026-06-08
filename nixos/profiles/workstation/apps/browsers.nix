7bc14ae331d218bb1c85ab8cf5b0ac4f34caa6ac
Artyom Tetyukhin <51746822+arttet@users.noreply.github.com>
2026-06-08 17:50:52 +0400

    feat(browser): enable Zen touchpad navigation

--- nixos/profiles/workstation/apps/browsers.nix
+++ nixos/profiles/workstation/apps/browsers.nix
@@ -4,6 +4,46 @@
   zen-browser,
   ...
 }:
+let
+  zenTouchpadPreferences = {
+    "apz.gtk.pangesture.enabled" = true;
+    "browser.gesture.swipe.left" = "Browser:BackOrBackDuplicate";
+    "browser.gesture.swipe.right" = "Browser:ForwardOrForwardDuplicate";
+    "browser.history_swipe_animation.disabled" = false;
+    "widget.disable-swipe-tracker" = false;
+  };
+
+  zenWithTouchpadGestures =
+    (pkgs.wrapFirefox zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default.unwrapped {
+      pname = "zen-browser";
+      extraPolicies.Preferences = {
+        "browser.gesture.swipe.left" = {
+          Value = zenTouchpadPreferences."browser.gesture.swipe.left";
+          Status = "default";
+        };
+        "browser.gesture.swipe.right" = {
+          Value = zenTouchpadPreferences."browser.gesture.swipe.right";
+          Status = "default";
+        };
+        "browser.history_swipe_animation.disabled" = {
+          Value = zenTouchpadPreferences."browser.history_swipe_animation.disabled";
+          Status = "default";
+        };
+        "widget.disable-swipe-tracker" = {
+          Value = zenTouchpadPreferences."widget.disable-swipe-tracker";
+          Status = "locked";
+        };
+      };
+      extraPrefs = ''
+        defaultPref("apz.gtk.pangesture.enabled", true);
+      '';
+    }).overrideAttrs
+      (old: {
+        passthru = (old.passthru or { }) // {
+          inherit zenTouchpadPreferences;
+        };
+      });
+in
 {
   boot.kernel.sysctl."user.max_user_namespaces" = 15000;
 
@@ -14,7 +54,7 @@
   };
 
   environment.systemPackages = [
-    zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
+    zenWithTouchpadGestures
     pkgs.brave
     pkgs.google-chrome
     pkgs.tor-browser
